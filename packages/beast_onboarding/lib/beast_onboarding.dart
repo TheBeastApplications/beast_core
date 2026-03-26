f6d17551304c9d060dcad3dd0633c941bb839418
Shuvam Jaswal <jaswalshuvam@gmail.com>
2026-03-26 13:29:01 +0530

    export added

--- packages/beast_onboarding/lib/beast_onboarding.dart
+++ packages/beast_onboarding/lib/beast_onboarding.dart
@@ -2,4 +2,5 @@ library;
 
 export 'src/providers/onboarding_provider.dart';
 export 'src/providers/onboarding_repository_provider.dart';
+export 'src/providers/onboarding_provider_extensions.dart';
 export 'src/repository/onboarding_repository.dart';
